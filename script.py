83ecb60bd6cce2c3ab18fd906ed54c46e84b3188
tap222 <mtapas.mohanty95@gmail.com>
2020-04-04 22:05:30 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import pandas as pd, numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -10,33 +14,30 @@ from sklearn.externals import joblib
 from scipy import sparse
 import gzip, re, string
 import pickle
+import argparse
 
 import warnings
 warnings.filterwarnings('ignore')
 
-# modelname=str(input())
-modelname ='------'
 
 root_dir = 'C:\\Users\\tamohant\\Desktop\\Auto_synthesis_Training_data\\' 
 
+modelname = input("Type model name : ")
+filename = input("Type file name : ")
 import os
 if not os.path.exists(root_dir + modelname):
-    os.makedirs(root_dir + modelname)
+	os.makedirs(root_dir + modelname)
 
 # Reading the file 
-train = pd.read_excel('----.xlsx',delimiter=',',encoding='latin-1')
-train['Description']=train['Short Description'].astype('str')
+train = pd.read_excel(filename,delimiter=',',encoding='latin-1')
+train['Description']=train['Ticket_Description'].astype('str')
 
-#Label columns
-# train['Intent'] = train[['Level1', 'Level2','Level3']].agg('-'.join, axis=1).astype('category')
-# train['Intent'] = train[['Level2','Level3']].agg('-'.join, axis=1).astype('category')
+train['Level2'] =  train['Level2'].astype('str')
+train['Level3'] =  train['Level3'].astype('str')
 
-train['Intent Level2'] =  train['Intent Level2'].astype('str')
-train['Intent Level3'] =  train['Intent Level3'].astype('str')
+train.dropna(subset = ['Level2', 'Level3'])
 
-train.dropna(subset = ['Intent Level2', 'Intent Level3'])
-
-train['Intent'] = train[['Intent Level2','Intent Level3']].agg('-'.join, axis=1).astype('category')
+train['Intent'] = train[['Level2','Level3']].agg('-'.join, axis=1).astype('category')
 train['Intent']= train['Intent'].astype('category')
 label_cols =  train['Intent'].cat.categories.tolist()
 
@@ -45,41 +46,37 @@ train = pd.concat([train,pd.get_dummies(train['Intent'])],axis=1)
 train.drop(['Intent'],axis=1, inplace=True)
 
 def pr(y_i, y):
-    p = x[y==y_i].sum(0)
-    return (p+1) / ((y==y_i).sum()+1)
+	p = x[y==y_i].sum(0)
+	return (p+1) / ((y==y_i).sum()+1)
 
 #Logistic Regression
 def get_mdl(y):
-    y = y.values
-    r = sparse.csr_matrix(np.log(pr(1,y) / pr(0,y)))
-    m = LogisticRegression(C=4,solver='sag',dual=False,class_weight="balanced",n_jobs=-1)
-    x_nb = x.multiply(r)
-    return m.fit(x_nb, y), r
-
-
-#Prediction
-
+	y = y.values
+	r = sparse.csr_matrix(np.log(pr(1,y) / pr(0,y)))
+	m = LogisticRegression(C=4,solver='sag',dual=False,class_weight="balanced",n_jobs=-1)
+	x_nb = x.multiply(r)
+	return m.fit(x_nb, y), r
 for name in label_cols:
-    COMMENT = 'Description'
-    train[COMMENT].fillna("unknown", inplace=True)
-    re_tok = re.compile(f'([{string.punctuation}“”¨_«»®´·º½¾¿¡§£₤‘’])')
-    def tokenize(s): return re_tok.sub(r' \1 ', s).split()
-    vec = TfidfVectorizer(ngram_range=(1,5),
-                          stop_words="english",
-                          analyzer='char',
-                          tokenizer=tokenize,
-                          min_df=3, max_df=0.9, 
-                          strip_accents='unicode',
-                          use_idf=1,
-                          smooth_idf=1, 
-                          sublinear_tf=1)
-    vec.fit(train[COMMENT])
-    x = vec.transform(train[COMMENT])
-    print('fit', name)
-    m,r = get_mdl(train[name])
-    model_loc = root_dir + modelname + '\\' + str(name).replace('/','or') + ".json.gz_model.pkl"
-    joblib.dump(m, model_loc)
-    r_loc = root_dir + modelname + '\\' +  str(name).replace('/','or') + ".json.gz_r.npz"
-    sparse.save_npz(r_loc, r)
-    vec_loc = root_dir + modelname + '\\' + str(name).replace('/','or') + ".json.gz_vector.pkl"
-    joblib.dump(vec, vec_loc,protocol=pickle.HIGHEST_PROTOCOL) 
+	COMMENT = 'Description'
+	train[COMMENT].fillna("unknown", inplace=True)
+	re_tok = re.compile(f'([{string.punctuation}“”¨_«»®´·º½¾¿¡§£₤‘’])')
+	def tokenize(s): return re_tok.sub(r' \1 ', s).split()
+	vec = TfidfVectorizer(ngram_range=(1,5),
+						  stop_words="english",
+						  analyzer='char',
+						  tokenizer=tokenize,
+						  min_df=3, max_df=0.9, 
+						  strip_accents='unicode',
+						  use_idf=1,
+						  smooth_idf=1, 
+						  sublinear_tf=1)
+	vec.fit(train[COMMENT])
+	x = vec.transform(train[COMMENT])
+	print('fit', name)
+	m,r = get_mdl(train[name])
+	model_loc = root_dir + modelname + '\\' + str(name).replace('/','or') + ".json.gz_model.pkl"
+	joblib.dump(m, model_loc)    
+	r_loc = root_dir + modelname + '\\' +  str(name).replace('/','or') + ".json.gz_r.npz"
+	sparse.save_npz(r_loc, r)
+	vec_loc = root_dir + modelname + '\\' + str(name).replace('/','or') + ".json.gz_vector.pkl"
+	joblib.dump(vec, vec_loc,protocol = pickle.HIGHEST_PROTOCOL)
